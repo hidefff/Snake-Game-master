53c49ef385454c62c98f4bfe67ee69242e3b2f97
red53star8 <88256219+red53star8@users.noreply.github.com>
2021-08-29 09:01:06 +0900

    creeated bomb .cpp and .h

---
+++ src/bomb.cpp
@@ -0,0 +1,15 @@
+#include "bomb.h"
+#include <cmath>
+#include <iostream>
+
+void Bomb::CreateBomb(){
+
+};
+
+void Bomb::ExplodeBomb(){
+
+}
+
+void Bomb::FinishBomb(){
+
+}
---
+++ src/bomb.h
@@ -0,0 +1,20 @@
+#ifndef BOMB_H
+#define BOMB_H
+
+#include <vector>
+#include "SDL.h"
+
+class Bomb {
+ public:
+ //Create Bomb
+ void CreateBomb();
+ //Explode Bomb
+ void ExplodeBomb();
+ //Finish Bomb
+ void FinishBomb();
+ 
+ private:
+
+};
+
+#endif
