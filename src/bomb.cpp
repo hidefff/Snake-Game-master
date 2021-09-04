0af00749b9429cbf532d4a2e71e1ccecb06882a2
red53star8 <88256219+red53star8@users.noreply.github.com>
2021-09-04 10:59:03 +0900

    successfully created random device for bomb

--- src/bomb.cpp
+++ src/bomb.cpp
@@ -1,21 +1,24 @@
 #include "bomb.h"
 #include <cmath>
 #include <iostream>
+#include <random>
 
 void Bomb::CreateBomb(){
     int x, y;
     while(true){
+
+        std::mt19937 engine2{ std::random_device{}() };
+        
         x = random_b_w(engine2);
         y = random_b_h(engine2);
-        
+
         bomb_x = x;
         bomb_y = y;
         
         return;
-        
+      
         //need to check that whether there is no snake and food
     }
-
     
 }
 
